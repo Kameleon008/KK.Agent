f47e7f74669e0aa34ae13561b884ef39b0f6fc85
Krzysztof Kochanowski <krzysztofkochanowski96@gmail.com>
2026-03-24 18:59:12 +0100

    chore: update configuration files and remove unused ConfigLogging

--- KK.Agent.Library/Configuration/Models/ConfigLogging.cs
+++
@@ -1,6 +0,0 @@
-namespace KK.Agent.Library.Configuration.Models;
-
-public class ConfigLogging
-{
-    public Dictionary<string, string> LogLevel { get; set; } = new();
-}
--- KK.Agent.Library/Configuration/Models/ConfigRoot.cs
+++ KK.Agent.Library/Configuration/Models/ConfigRoot.cs
@@ -2,7 +2,5 @@ namespace KK.Agent.Library.Configuration.Models;
 
 public class ConfigRoot
 {
-    public ConfigLogging Logging { get; set; } = new();
-
     public ConfigProvider Provider { get; set; } = new();
 }
