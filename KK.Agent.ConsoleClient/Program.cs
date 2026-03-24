0efa816ad53dc01f8129c3d67add3da673aa4720
Krzysztof Kochanowski <krzysztofkochanowski96@gmail.com>
2026-03-24 18:11:39 +0100

    chore: init repo with Visual Studio .gitignore

---
+++ KK.Agent.ConsoleClient/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace KK.Agent.ConsoleClient;
+
+public static class Program
+{
+    public static async Task Main(string[] args)
+    {
+        await Task.Delay(100);
+
+        Console.WriteLine("Hello World!");
+    }
+}
